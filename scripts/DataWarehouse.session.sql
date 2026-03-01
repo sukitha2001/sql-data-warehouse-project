ced9fc2d7b51df46a17db4e371edbc6512f01e1a
Sukitha Rathnayake <sukitharathnayake001@gmail.com>
2026-03-01 19:03:42 +0530

    Updated scema creation

--- scripts/DataWarehouse.session.sql
+++ scripts/DataWarehouse.session.sql
@@ -1,4 +1,17 @@
+/*
+Create Database and Schemas
+Script Purpose:
+This script creates a new database named 'DataWarehouse' after checking if it already exists.
+If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas within the database: 'bronze', 'silver', and 'gold'.
+WARNING:
+Running this script will drop the entire 'Datawarehouse' database if it exists.
+All data in the database will be permanently deleted. Proceed with caution and ensure you have proper backups before running this script.
+*/
 
+-- Drop the database if it exists, forcing disconnection of other users (Requires Postgres 13+)
+DROP DATABASE IF EXISTS "DataWarehouse" WITH (FORCE);
+
+-- Create the database
 CREATE DATABASE "DataWarehouse";
 
 CREATE SCHEMA bronze;
@@ -7,6 +20,7 @@ CREATE SCHEMA silver;
 GO
 CREATE SCHEMA gold;
 GO
+
 
 
 
