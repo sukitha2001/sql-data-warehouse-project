1816fc00212da0611b5a216f44701dcd45e40a28
Sukitha Rathnayake <sukitharathnayake001@gmail.com>
2026-03-03 23:45:46 +0530

    how to use comment update

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,6 +1,9 @@
--- ==========================================
--- DDL Script: Create Bronze Layer Tables
--- ==========================================
+/*
+DDL Script: Create Bronze Tables
+Script Purpose:
+This script creates tables in the 'bronze' schema, dropping existing tables if they already exist.
+Run this script to re-define the DDL structure of 'bronze' Tables
+*/
 
 CREATE SCHEMA IF NOT EXISTS bronze;
 
--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,6 +1,16 @@
--- ==========================================
--- Procedure Script: Load Bronze Layer
--- ==========================================
+/*
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+Script Purpose:
+This stored procedure loads data into the 'bronze' schema from external CSV files.
+It performs the following actions:
+- Truncates the bronze tables before loading data.
+- Uses the 'BULK INSERT' command to load data from csv Files to bronze tables.
+Parameters:
+None.
+This stored protedure does not accept any parameters or return any values.
+Usage Example:
+EXEC bronze.load_bronze;
+*/
 
 CREATE OR REPLACE PROCEDURE bronze.load_bronze()
 LANGUAGE plpgsql
