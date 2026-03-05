a8ddb355111f94efcc7946db5a0dc3f7b7528d89
Sukitha Rathnayake <sukitharathnayake001@gmail.com>
2026-03-06 01:02:23 +0530

    sales table

--- scripts/silver/data_quality.sql
+++ scripts/silver/data_quality.sql
@@ -75,3 +75,48 @@ WHERE prd_key IN ('AC-HE-HL-U509-R', 'AC-HE-HL-U509');
 
 
 SELECT * FROM silver.crm_prd_info;
+
+
+SELECT
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+sls_order_dt,
+sls_ship_dt,
+sls_due_dt,
+sls_sales,
+sls_quantity, 
+sls_price
+FROM bronze.crm_sales_details
+WHERE sls_prd_key NOT IN (SELECT prd_key FROM silver.crm_prd_info);
+
+
+SELECT NULLIF (sls_order_dt, 0) AS sls_order_dt FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0 OR LENGTH(sls_order_dt::TEXT) != 8;
+
+
+-- checking for invalid date formats in the sls_order_dt, sls_ship_dt, and sls_due_dt columns. The expectation is that there should be no results returned, indicating that all date values are in the correct format and can be successfully converted to date data types.
+
+SELECT * FROM bronze.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt OR sls_ship_dt > sls_due_dt;
+
+
+-- » Check Data Consistency: Between Sales, Quantity, and Price 
+-- » sales = Quantity * Price
+-- » Values must not be NULL, zero, or negative.
+
+select distinct
+sls_sales,
+sls_quantity,
+sls_price
+from bronze.crm_sales_details
+where sls_sales != sls_quantity * sls_price
+or sls_sales <= 0
+or sls_quantity <= 0
+or sls_price <= 0
+or sls_sales IS NULL
+or sls_quantity IS NULL
+or sls_price IS NULL
+ORDER BY sls_sales, sls_quantity, sls_price;
+
+
